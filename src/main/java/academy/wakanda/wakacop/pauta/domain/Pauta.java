1d9a431b93a2921503560279f2f03c843ff16cd5
Eliseu Divino Santos <eliseusantico@gmail.com>
2023-10-23 23:43:42 -0300

    implementa domain pauta

---
+++ src/main/java/academy/wakanda/wakacop/pauta/domain/Pauta.java
@@ -0,0 +1,23 @@
+package academy.wakanda.wakacop.pauta.domain;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Entity
+@ToString
+
+public class Pauta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "uuid", updatable = false, unique = true, nullable = false)
+    private UUID id;
+    private String titulo;
+    private String descricao;
+    private UUID idAssociadoAutor;
+    private LocalDateTime dataCriacao;
+}
