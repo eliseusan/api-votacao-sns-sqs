aed74f302511ff0c56d7b4ca0b56093e01b50db2
Eliseu Divino Santos <eliseusantico@gmail.com>
2023-11-08 21:36:45 -0300

    implementa sessao votacao

--- src/main/java/academy/wakanda/wakacop/sessaovotacao/application/api/SessaoVotacaoAPI.java
+++ src/main/java/academy/wakanda/wakacop/sessaovotacao/application/api/SessaoVotacaoAPI.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/sessaovotacao")
+@RequestMapping("/sessao/abertura")
 public interface SessaoVotacaoAPI {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
